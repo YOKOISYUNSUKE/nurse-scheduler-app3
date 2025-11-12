be0a3fc9eef4e1655242603373f3141745e2f82f
YOKOISYUNSUKE <yokoisyunsuke@gmail.com>
2025-11-12 16:16:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const VERSION = 'v2025-11-12.4';
+const VERSION = 'v2025-11-12.5';
 const STATIC_CACHE = `static-${VERSION}`;
 const ENTRY_HTML = './index.html'; // ← `!doctype (1).html` のまま使うなら './!doctype (1).html' に変更
 
