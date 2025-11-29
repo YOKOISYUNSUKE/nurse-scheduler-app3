27c31c343216eb21b1160a7172fd0edc772e8647
YOKOISYUNSUKE <yokoisyunsuke@gmail.com>
2025-11-29 22:00:44 +0900

    Remove PWA registration code

--- js/pwa-register.js
+++ js/pwa-register.js
@@ -1,11 +1,1 @@
-// js/pwa-register.js
 
-document.addEventListener("DOMContentLoaded", () => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("../service-worker.js")
-      .catch((err) => {
-        console.warn("ServiceWorker registration failed:", err);
-      });
-  }
-});
